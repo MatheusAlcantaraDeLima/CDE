605a9543d4ab62abf624c1a8b63c7417ffd3095d
Matheus Alcântara <matheusalcantaradelima1997@gmail.com>
2020-07-22 07:17:01 -0300

    criado a página de login

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.css">
+    <title>Login</title>
+</head>
+<body class="fundo">
+    <div class="container">
+        <div class="caixa">
+            <div class="row">
+                <div class="col">
+                    <img src="img/delivery.png" alt="carrinho com caixa" class="img-fluid col-8 mt-5 ml-5">
+                </div>
+                <div class="col mt-4">
+                    <h1 class="texto">Bem Vindo(a)</h1>
+                    <div class="form-group mr-4">
+                        <input type="text" placeholder="name" class="form-control mt-4 border-0 shadow-sm">
+                        <input type="password" placeholder="password" class="form-control mt-2 border-0 shadow-sm">
+                        <div class="row">
+                            <div class="col">
+                                <button type="button" class="btn btn-sm fundo2 mt-3 border-0 texto col-12 text-white">Login</button>
+                            </div>
+                            <div class="col">
+                                <button type="button" class="btn btn-sm mt-3 border-0 texto col-12">Forget Password</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</body>
+</html>
